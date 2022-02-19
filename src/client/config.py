7cf66665bdb005441f740ab46e95f8bce943521e
Stephen Betts <sneakybVer@users.noreply.github.com>
2022-02-19 15:01:59 +0000

    Delete config.py

--- src/client/config.py
+++
@@ -1,8 +0,0 @@
-DARWIN_WEBSERVICE_NAMESPACE = (
-    "com",
-    "http://thalesgroup.com/RTTI/2010-11-01/ldb/commontypes",
-)
-DARWIN_TOKEN = "c3298d2f-9ac8-43dd-bfb6-ba071609ec01"
-LDBWS_URL = (
-    "https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2016-02-16"
-)
